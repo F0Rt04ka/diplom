94bfd7b13c69296234e97229066ddadefe47f837
Solovev Roman <rmn.2007@mail.ru>
2019-05-17 00:27:28 +0700

    Add JQuery regex selector

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,19 @@
 const $ = require('jquery');
 window.jQuery = window.$ = $;
 
+$.expr[':'].regex = function(elem, index, match) {
+    let matchParams = match[3].split(','),
+        validLabels = /^(data|css):/,
+        attr = {
+            method: matchParams[0].match(validLabels) ?
+                matchParams[0].split(':')[0] : 'attr',
+            property: matchParams.shift().replace(validLabels,'')
+        },
+        regexFlags = 'ig',
+        regex = new RegExp(matchParams.join('').replace(/^\s+|\s+$/g,''), regexFlags);
+    return regex.test(jQuery(elem)[attr.method](attr.property));
+};
+
 const routes = require('../../public/js/fos_js_routes.json');
 const Routing = require('../../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.min.js');
 Routing.setRoutingData(routes);
