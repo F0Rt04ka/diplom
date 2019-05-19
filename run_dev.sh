b8f155f475f67407ad75a41d4f53f064775f4e6e
Solovev Roman <rmn.2007@mail.ru>
2019-05-19 18:00:53 +0700

    Update run command

--- run_dev.sh
+++ run_dev.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
+select=$1;
 
-#php bin/console server:start 0.0.0.0:8000
-php bin/console server:start
-docker-compose up -d
+[ -z "$select" ] && select="dev"
+
+if [ "$select" = "dev" -o "$select" = "global" -o "$select" = "stop" ]
+then
+    if [ "$select" = "dev" ]
+    then
+        php bin/console server:start
+        docker-compose up -d
+    fi
+
+    if [ "$select" = "global" ]
+    then
+        php bin/console server:start 0.0.0.0:8000
+        docker-compose up -d
+    fi
+
+    if [ "$select" = "stop" ]
+    then
+        php bin/console server:stop
+        docker-compose stop
+    fi
+else
+    echo "Wrong argument. Allowed only [dev, global, stop]";
+fi
